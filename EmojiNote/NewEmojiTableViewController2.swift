39544f38f81793e110c05732122f176e6c8063f0
AleksandrKotliar <sashagsmnew@gmail.com>
2022-11-30 14:21:13 +0400

    Setup Second Screen Interface

---
+++ EmojiNote/NewEmojiTableViewController2.swift
@@ -0,0 +1,34 @@
+//
+//  NewEmojiTableViewController2.swift
+//  EmojiNote
+//
+//  Created by macbook on 30.11.22.
+//
+
+import UIKit
+
+class NewEmojiTableViewController2: UITableViewController {
+
+    @IBOutlet weak var emojiTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func textChanged(_ sender: UITextField) {
+        
+        
+        
+        
+    }
+}
